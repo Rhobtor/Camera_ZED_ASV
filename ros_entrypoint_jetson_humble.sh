4388f9abcbd4b1b8c90ad692f54f6a455a977bda
rhobtor <luismidp7@gmail.com>
2024-03-07 21:36:10 +0100

    update

--- ros_entrypoint_jetson_humble.sh
+++ ros_entrypoint_jetson_humble.sh
@@ -17,6 +17,6 @@ echo "---------------------"
 
 # Run your command in the background
 # ros2 run tf2_ros static_transform_publisher 0 0.06 0 0 0 0 base_link zed2i_camera_center &
-ros2 launch zed_wrapper zed_camera.launch.py camera_model:='zed2i' camera_name:='zed'
+# ros2 launch zed_wrapper zed_camera.launch.py camera_model:='zed2i' camera_name:='zed'
 
 exec "$@"
